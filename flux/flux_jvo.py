925b3a53ea94b9bb425fe6822ab6497d9aefc0c9
leblog92 <50671954+leblog92@users.noreply.github.com>
2026-04-09 16:10:27 +0200

    Update flux_jvo.py

--- flux/flux_jvo.py
+++ flux/flux_jvo.py
@@ -996,9 +996,9 @@ document.getElementById('ttsText').addEventListener('keydown', e => {{
 const QUICK_PHRASES = [
   'Bonjour, bienvenue dans la salle jeux vidéo.',
   'Un membre du personnel va arriver dans quelques instants. Veuillez patienter.',
-  'Vous pouvez vous installer dans la salle du fond.',
+  'Vous pouvez vous installer dans la salle.',
   'Attention, la salle fermera dans quelques minutes !',
-  'Vous pouvez consulter les jeux disponibles sur place sur le panneau.',
+  'Les jeux disponibles sur place présentés sur le panneau.',
 ];
 function buildQuickBtns() {{
   const c = document.getElementById('quickBtns');
